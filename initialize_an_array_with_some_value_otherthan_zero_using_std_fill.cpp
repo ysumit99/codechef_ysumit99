137ef025435b5b342d99df179867d2199ed33cc0
Sumit Yadav <ysumit99@gmail.com>
2017-06-25 16:24:00 +0530

    initialize an array with any value using std::fill in c++ stl

---
+++ initialize_an_array_with_some_value_otherthan_zero_using_std_fill.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int array[100];
+
+	std::fill(array, array+100, -1);
+	cout<<array[99]<<endl;
+	cout<<array[59]<<endl;
+	cout<<array[75]<<endl;
+	cout<<array[37]<<endl;
+	cout<<array[14]<<endl;
+	cout<<array[2]<<endl;
+
+	return 0;
+}
